d492198aac4306c075a2294c1158cea808557754
Evan Dwi  A. <evanpw973@gmail.com>
2026-07-29 19:35:29 +0700

    Add Category and Transaction resources with CRUD functionality

---
+++ app/Filament/Resources/Transactions/Schemas/TransactionForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Transactions\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class TransactionForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('total')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('pay_total')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
